7ee02baffbc6724297574684a4520e66feb840f4
enesJav16 <maratuenes@gmail.com>
2024-10-17 11:19:36 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,31 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Flowers flower1 = new Flowers();
+        flower1.name ="Rose";
+        flower1.freshness= 2;
+        flower1.price=100;
+
+        Flowers flower2 = new Flowers();
+        flower2.name ="Lyly";
+        flower2.freshness= 6;
+        flower2.price=200;
+
+        Flowers flower3 = new Flowers();
+        flower3.name ="Moon-flower";
+        flower3.freshness= 4;
+        flower3.price=300;
+
+
+        Flowers [] flowers={flower1,flower2,flower3};
+
+        Flowers freshList = new Flowers();
+        freshList.getFreshness(flowers);
+
+        System.out.println("");
+
+        Flowers priceList = new Flowers();
+        priceList.getPrice(flowers);
     }
 }
